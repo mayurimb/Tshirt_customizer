dd7a19121d12205acb343fa2a82b698a06f4622c
Mayuri Bashirabadkar <114315491+mayurimb@users.noreply.github.com>
2024-08-13 00:24:07 -0400

    config

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,5 +1,9 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+const REACT_APP_BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 const config = {
-    backendUrl: process.env.REACT_APP_BACKEND_URL ||"http://localhost:3000/api/sdiffusion",
+    backendUrl: `${REACT_APP_BACKEND_URL}api/sdiffusion` ||"http://localhost:3000/api/sdiffusion",
 };
 
 export default config;
