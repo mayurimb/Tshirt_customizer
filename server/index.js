77e9e494822fce99dd0e9d4a8ac0240a5b3f7509
Mayuri Bashirabadkar <114315491+mayurimb@users.noreply.github.com>
2024-08-13 10:35:10 -0400

    CORS policy changes

--- server/index.js
+++ server/index.js
@@ -1,16 +1,19 @@
 import express from 'express';
 import cors from 'cors';
 import sdiffusionRoutes from './routes/sdiffusion.routes.js';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors({
     origin: ['https://t-customizer.vercel.app'],
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization',
   }));
-
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "50mb" }))
 
 app.use("/api/sdiffusion", sdiffusionRoutes);
-
 app.listen(3000, () => console.log(`Server has started on port ${port}`))
