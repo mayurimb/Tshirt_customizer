606c506f1aabeb527cbb172d3e1db90d3be3f25a
Mayuri Bashirabadkar <114315491+mayurimb@users.noreply.github.com>
2024-08-13 10:29:11 -0400

    index.js

--- server/index.js
+++ server/index.js
@@ -1,15 +1,11 @@
 import express from 'express';
-//import * as dotenv from 'dotenv';
 import cors from 'cors';
 import sdiffusionRoutes from './routes/sdiffusion.routes.js';
 
-//dotenv.config();
 const app = express();
-//const port = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: '*', // Allows requests from any origin. You can restrict this to specific origins if needed.
-    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    origin: ['https://t-customizer.vercel.app'],
     allowedHeaders: 'Content-Type,Authorization',
   }));
 
