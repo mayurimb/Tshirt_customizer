cd58689d7d1c79981da876e16193d813fc5283ab
Mayuri Bashirabadkar <114315491+mayurimb@users.noreply.github.com>
2024-08-13 00:27:57 -0400

    index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import * as dotenv from 'dotenv';
+//import * as dotenv from 'dotenv';
 import cors from 'cors';
 import sdiffusionRoutes from './routes/sdiffusion.routes.js';
 
-dotenv.config();
+//dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
+//const port = process.env.PORT || 3000;
 
 app.use(cors({
     origin: '*', // Allows requests from any origin. You can restrict this to specific origins if needed.
@@ -17,4 +17,4 @@ app.use(express.json({ limit: "50mb" }))
 
 app.use("/api/sdiffusion", sdiffusionRoutes);
 
-app.listen(port, () => console.log(`Server has started on port ${port}`))
+app.listen(3000, () => console.log(`Server has started on port ${port}`))
