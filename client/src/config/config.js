44693b9321d834ca4b9403e1267df4239d93488e
Mayuri Bashirabadkar <114315491+mayurimb@users.noreply.github.com>
2024-08-13 00:44:04 -0400

    link updated

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,9 +1,5 @@
-import * as dotenv from 'dotenv';
-
-dotenv.config();
-const REACT_APP_BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 const config = {
-    backendUrl: `${REACT_APP_BACKEND_URL}api/sdiffusion` ||"http://localhost:3000/api/sdiffusion",
+    backendUrl:"https://tshirt-customizer-orcin.vercel.app/api/sdiffusion" ||"http://localhost:3000/api/sdiffusion",
 };
 
 export default config;
