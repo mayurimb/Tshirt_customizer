5c2d0f096c08db4b9fc104191ae4fb70e4c61dce
Mayuri Bashirabadkar <114315491+mayurimb@users.noreply.github.com>
2024-08-13 21:41:58 -0400

    cors

--- server/index.js
+++ server/index.js
@@ -12,20 +12,9 @@ app.use(cors({
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization',
   }));
-//app.use(cors(corsOptions));
+app.options('*', cors());
 app.use(express.json({ limit: "50mb" }))
 app.use("/api/sdiffusion", sdiffusionRoutes);
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') {
-      res.sendStatus(200);
-  } else {
-      next();
-  }
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server has started on port ${PORT}"))
